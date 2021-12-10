35db908fe7c94efee1547facc25d09f32cf14c38
joaomarcuscardoso <11190883996@edu.udesc.br>
2021-12-10 13:53:52 -0300

    Add all Methods test in AnimeController

--- src/main/java/academy/devdojo/springboot2/requests/AnimePostRequestBody.java
+++ src/main/java/academy/devdojo/springboot2/requests/AnimePostRequestBody.java
@@ -1,11 +1,13 @@
 package academy.devdojo.springboot2.requests;
 
+import lombok.Builder;
 import lombok.Data;
 import org.hibernate.validator.constraints.URL;
 
 import javax.validation.constraints.NotEmpty;
 
 @Data
+@Builder
 public class AnimePostRequestBody {
     // Validation
     @NotEmpty(message = "The anime name cannot be empty")

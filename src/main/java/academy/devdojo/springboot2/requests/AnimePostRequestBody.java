8d9168362db2e9466ff8edbe15a22d14cbfa38c6
joaomarcuscardoso <11190883996@edu.udesc.br>
2021-12-11 18:02:40 -0300

    Tests Integrations in the AnimeController 2/2

--- src/main/java/academy/devdojo/springboot2/requests/AnimePostRequestBody.java
+++ src/main/java/academy/devdojo/springboot2/requests/AnimePostRequestBody.java
@@ -1,13 +1,17 @@
 package academy.devdojo.springboot2.requests;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.URL;
 
 import javax.validation.constraints.NotEmpty;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class AnimePostRequestBody {
     // Validation
     @NotEmpty(message = "The anime name cannot be empty")

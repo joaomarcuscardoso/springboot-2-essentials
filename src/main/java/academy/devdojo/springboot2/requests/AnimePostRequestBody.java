df9a67997787c890c3c41dcbd07b14af68cb0539
joaomarcuscardoso <11190883996@edu.udesc.br>
2021-12-12 19:15:18 -0300

    Swagger Documentation 2/2

--- src/main/java/academy/devdojo/springboot2/requests/AnimePostRequestBody.java
+++ src/main/java/academy/devdojo/springboot2/requests/AnimePostRequestBody.java
@@ -1,10 +1,10 @@
 package academy.devdojo.springboot2.requests;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.validator.constraints.URL;
 
 import javax.validation.constraints.NotEmpty;
 
@@ -15,8 +15,7 @@ import javax.validation.constraints.NotEmpty;
 public class AnimePostRequestBody {
     // Validation
     @NotEmpty(message = "The anime name cannot be empty")
+    @Schema(description = "This is the Anime's name", example = "One Piece", required=true)
     private String name;
-    @URL(message=" The URL is not valid")
-    private String url;
 
 }
